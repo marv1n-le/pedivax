877dc6ee91ac1ccb906e6005e9a3c38c7fc380d4
duc-anh-le <ducanhle.iter@gmail.com>
2025-02-21 14:59:37 +0700

    config appsettings

--- PediVax.API/Program.cs
+++ PediVax.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using PediVax.BusinessObjects.DBContext;
+
 namespace PediVax;
 
 public class Program
@@ -12,6 +15,9 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+
+        builder.Services.AddDbContext<PediVaxContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
         var app = builder.Build();
 

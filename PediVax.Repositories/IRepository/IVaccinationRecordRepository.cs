e78413b6ba4111ac070c65ad0e999e69d29b75b3
hoangleminhtriet.bonzy <hoangleminhtriet@gmail.com>
2025-03-04 12:17:33 +0700

    func: update vaccination record

--- PediVax.Repositories/IRepository/IVaccinationRecordRepository.cs
+++ PediVax.Repositories/IRepository/IVaccinationRecordRepository.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PediVax.BusinessObjects.Models;
+using System.Threading;
 
 namespace PediVax.Repositories.IRepository
 {
-    interface Interface1
+    public interface IVaccinationRecordRepository
     {
+        Task<List<VaccinationRecord>> GetAllVaccinationRecords(CancellationToken cancellationToken);
+        Task<(List<VaccinationRecord> Data, int TotalCount)> GetVaccinationRecordsPaged(int pageNumber, int pageSize, CancellationToken cancellationToken);
+        Task<VaccinationRecord> GetVaccinationRecordById(int id, CancellationToken cancellationToken);
+        Task<int> AddVaccinationRecord(VaccinationRecord vaccinationRecord, CancellationToken cancellationToken);
+        Task<int> UpdateVaccinationRecord(VaccinationRecord vaccinationRecord, CancellationToken cancellationToken);
+        Task<bool> DeleteVaccinationRecord(int id, CancellationToken cancellationToken);
     }
 }
--- PediVax.Services/IService/IVaccinationRecordService.cs
+++ PediVax.Services/IService/IVaccinationRecordService.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PediVax.BusinessObjects.DTO;
+using PediVax.BusinessObjects.DTO.VaccinationRecordDTo;
+using PediVax.BusinessObjects.Models;
+using System.Threading;
 
 namespace PediVax.Services.IService
 {
-    interface IVaccinationRecordService
+    public interface IVaccinationRecordService
     {
+        Task<List<VaccinationRecordRequestDTO>> GetAllVaccinationRecords(CancellationToken cancellationToken);
+        Task<VaccinationRecordRequestDTO> GetVaccinationRecordById(int recordId, CancellationToken cancellationToken);
+        Task<(List<VaccinationRecordRequestDTO> Data, int TotalCount)> GetVaccinationRecordsPaged(int pageNumber, int pageSize, CancellationToken cancellationToken);
+        Task<int> AddVaccinationRecord(CreateVaccinationRecordDTO createVaccinationRecordDTO, CancellationToken cancellationToken);
+        Task<bool> UpdateVaccinationRecord(int recordId, UpdateVaccinationRecordDTO updateVaccinationRecordDTO, CancellationToken cancellationToken);
+        Task<bool> DeleteVaccinationRecord(int recordId, CancellationToken cancellationToken);
     }
 }

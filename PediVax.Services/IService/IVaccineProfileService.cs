a76afe0151cb38d0b21ef894ec779396c53c1dfd
duc-anh-le <ducanhle.iter@gmail.com>
2025-03-10 22:24:03 +0700

    func: update for vaccineprofile

--- PediVax.Services/IService/IVaccineProfileService.cs
+++ PediVax.Services/IService/IVaccineProfileService.cs
@@ -12,7 +12,7 @@ namespace PediVax.Services.IService
         Task<List<VaccineProfileResponseDTO>> GetAllVaccineProfiles(CancellationToken cancellationToken);
         Task<VaccineProfileResponseDTO> GetVaccineProfileById(int id, CancellationToken cancellationToken);
         Task<VaccineProfileResponseDTO> AddVaccineProfile(CreateVaccineProfileDTO vaccineProfileDTO, CancellationToken cancellationToken);
-        //Task<int> UpdateVaccineProfile(UpdateVaccineProfileDTO vaccineProfile, CancellationToken cancellationToken);
+        Task<bool> UpdateVaccineProfile(int id, UpdateVaccineProfileDTO updateVaccineProfileDTO, CancellationToken cancellationToken);
         Task<bool> DeleteVaccineProfile(int id, CancellationToken cancellationToken);
 
     }
